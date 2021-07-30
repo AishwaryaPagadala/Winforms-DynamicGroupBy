e60088a18ba8295fb47bbec3a76674f81e2db8e7
AishwaryaPagadala <84288700+AishwaryaPagadala@users.noreply.github.com>
2021-07-30 23:27:19 +0530

    Add files via upload

---
+++ groupingAssignment1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load = new System.Windows.Forms.Button();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.entityInstantFeedbackSource1 = new DevExpress.Data.Linq.EntityInstantFeedbackSource();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // load
+            // 
+            this.load.BackColor = System.Drawing.Color.White;
+            this.load.Location = new System.Drawing.Point(245, 24);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 23);
+            this.load.TabIndex = 0;
+            this.load.Text = "Load";
+            this.load.UseVisualStyleBackColor = false;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // table
+            // 
+            this.table.BackgroundColor = System.Drawing.Color.White;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Location = new System.Drawing.Point(52, 53);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(976, 271);
+            this.table.TabIndex = 1;
+            this.table.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.table_CellContentClick);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(1061, 53);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1061, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "group";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 348);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(976, 241);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1504, 794);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.DataGridView table;
+        private CheckedListBox checkedListBox1;
+        private DevExpress.Data.Linq.EntityInstantFeedbackSource entityInstantFeedbackSource1;
+        private Button button1;
+        private RichTextBox textBox2;
+    }
+}
+
---
+++ groupingAssignment1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=PC-HYD-DEV014;Initial Catalog=assignment;Persist Security Info=True;User ID=sa;Password=Password123");
+        SqlCommand com;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            try
+            {
+
+                com = new SqlCommand
+                {
+                    Connection = con,
+                    CommandText = "select * from employee"
+                };
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        checkedListBox1.Items.Add(column.ColumnName, true);
+                    }
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                com = new SqlCommand
+                {
+                    Connection = con,
+                    CommandText = "select * from employee"
+                };
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    table.DataSource = dt;
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        
+        private void table_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+
+        private void checkedListBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+     
+            try
+            {
+                textBox2.Clear();
+                List<string> columnsSelected = new List<string>();
+                string groupingColumns = string.Empty;
+                com = new SqlCommand
+                {
+                    Connection = con,
+                    CommandText = "select * from employee"
+                };
+                con.Open();
+                foreach (var columnNames in checkedListBox1.CheckedItems)
+                { 
+                    columnsSelected.Add(columnNames.ToString());
+                }
+                SqlDataReader reader = com.ExecuteReader();
+                DataTable dtEmployees = new DataTable();
+                if (reader.HasRows)
+                {
+
+                    dtEmployees.Load(reader);
+                    table.DataSource = dtEmployees;
+                }
+                DataView dvEmployees = dtEmployees.DefaultView;
+                DataTable dtSortedData = dvEmployees.ToTable();
+                DataTable uniqueCols = dvEmployees.ToTable(true,columnsSelected.ToArray());
+                string[] selectClause=new string[uniqueCols.Rows.Count];
+                int iterator = 0;
+ 
+                foreach (DataRow drr in uniqueCols.Rows)
+                {
+                    foreach (DataColumn dcc in uniqueCols.Columns)
+                    {
+                      
+                        selectClause[iterator] = selectClause[iterator] + dcc.ColumnName + "=" + "'" + drr[dcc] + "'" +" "+ "and"+" ";
+
+                    }
+                    selectClause[iterator]=selectClause[iterator].Substring(0, selectClause[iterator].Length - 4);
+                    iterator++;
+                }
+                string temp;
+                for (int selectclausevalue = 0; selectclausevalue < selectClause.Length; selectclausevalue++)
+                {
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText(selectClause[selectclausevalue]);
+                    textBox2.AppendText(Environment.NewLine);
+                
+                    foreach (var rowsMatched in dtSortedData.Select(selectClause[selectclausevalue]))
+                    { temp = string.Empty;
+                        foreach (DataColumn dtc in dtSortedData.Columns)
+                        {
+                            temp = temp + rowsMatched[dtc]+" ";
+                        }
+                        textBox2.AppendText("\t");
+                        textBox2.AppendText(temp);
+                        textBox2.AppendText(Environment.NewLine);
+                    }
+                }
+              
+
+
+              
+            }
+            finally
+            {
+               
+                con.Close();
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+   
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
